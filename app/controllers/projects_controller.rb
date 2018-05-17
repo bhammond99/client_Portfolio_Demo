6eeae669fc98972adcd2ea7ed5f905a2cf2f3374
brycehammond <hammond.bryce99@gmail.com>
2018-05-16 20:37:25 -0400

    added edit and update functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,13 +8,30 @@ class ProjectsController < ApplicationController
 	end
 
 	def create
-    @project_item = Project.new(params.require(:project).permit(:title, :subtitle, :body))
+	    @project_item = Project.new(params.require(:project).permit(:title, :subtitle, :body))
+
+	    respond_to do |format|
+	      if @project_item.save
+	        format.html { redirect_to projects_path, notice: 'Project successfully created.' }
+	      else
+	        format.html { render :new }
+	      end
+	    end
+  end
+
+  def edit
+  	@project_item = Project.find(params[:id])
+  end	
+
+  def update
+  	@project_item = Project.find(params[:id])
 
     respond_to do |format|
-      if @project_item.save
-        format.html { redirect_to projects_path, notice: 'Project successfully created.' }
+      if @project_item.update(params.require(:project).permit(:title, :subtitle, :body))
+        format.html { redirect_to projects_path, notice: 'The Project was successfully updated.' }
+
       else
-        format.html { render :new }
+        format.html { render :edit }
       end
     end
   end
