71555b4b9e04575c4181a2a0dd6e663a55dd739d
brycehammond <hammond.bryce99@gmail.com>
2018-05-28 11:32:55 -0400

    custom project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :projects
+	#below code: brings in all of the resource routes except show
+
+  resources :projects, except: [:show]
+  get 'project/:id', to: 'projects#show', as: 'project_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
