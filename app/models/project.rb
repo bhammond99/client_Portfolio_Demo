0a90465176aa3d2a6f7af858e58ac9fcbd82a8d3
brycehammond <hammond.bryce99@gmail.com>
2018-07-17 14:54:23 -0400

    Implemented image defaults for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,12 @@ validates_presence_of :title, :body, :main_image, :thumb_image
 	end	
 
 	scope :ruby_on_rails_project_items, -> { where(subtitle: 'Ruby on Rails') }
+
+	after_initialize :set_defaults 
+
+	def set_defaults
+		self.main_image ||= "https://placeholdit.co//i/600x400"
+		self.thumb_image ||= "https://placeholdit.co//i/350x200"
+	end	
 end
+
