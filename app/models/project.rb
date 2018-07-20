f8c06704723ec4de8fe815ce6ed372db57c77ca3
brycehammond <hammond.bryce99@gmail.com>
2018-07-19 21:47:52 -0400

    Added concerns for data management

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
-validates_presence_of :title, :body, :main_image, :thumb_image
+	include Placeholder
+	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	def self.angular
 		where(subtitle: 'Angular')
@@ -10,8 +11,8 @@ validates_presence_of :title, :body, :main_image, :thumb_image
 	after_initialize :set_defaults 
 
 	def set_defaults
-		self.main_image ||= "https://placeholdit.co//i/600x400"
-		self.thumb_image ||= "https://placeholdit.co//i/350x200"
+		self.main_image ||= Placeholder.image_generator(height:'600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height:'350', width: '200')
 	end	
 end
 
